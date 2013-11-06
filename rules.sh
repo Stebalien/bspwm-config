863573484579f1e5653e01680b872ed132f49c8c
Steven Allen <steven@stebalien.com>
2013-11-05 22:42:05 -0500

    Rule script formatting.

--- rules.sh
+++ rules.sh
@@ -13,17 +13,20 @@ case "$TYPE" in
     desktop)                    RULE+=("lower=on")      ;;
 esac
 
-case "$INST" in
-    pithos) RULE+=("desktop=$([[ $(bspc query -M | wc -l) -gt 1 ]] && echo '^1' || echo 'next.free')") ;;
-    mpv|keepassx) RULE+=("floating=on") ;;
-    dropterm|crx_nckgahadagoaajjgafhacjanaoiihapd) RULE+=("floating=on sticky=on") ;;
-esac
-
 for s in $STATE; do
     case $s in
         sticky)                 RULE+=("sticky=on")     ;;
         fullscreen)             RULE+=("fullscreen=on") ;;
     esac
 done
+
+case "$INST" in
+    pithos)
+        RULE+=("desktop=$([[ $(bspc query -M | wc -l) -gt 1 ]] && echo '^1' || echo 'next.free')") ;;
+    mpv|keepassx)
+        RULE+=("floating=on") ;;
+    dropterm|crx_nckgahadagoaajjgafhacjanaoiihapd)
+        RULE+=("floating=on sticky=on") ;;
+esac
 
 echo "${RULE[*]}"
