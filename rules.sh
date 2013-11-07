def26150a71147ef34bbef7f2c0f7c305fb78d7e
Steven Allen <steven@stebalien.com>
2013-11-07 18:12:14 -0500

    bspwm now has sane mapping defaults. don't handle them here.

--- rules.sh
+++ rules.sh
@@ -1,34 +1,11 @@
 #!/bin/bash
-readarray -t PROPS < <(xwinfo -its $1)
 
-INST=${PROPS[0]}
-TYPE=${PROPS[1]}
-STATE=(${PROPS[2]})
-
-RULE=()
-
-case "$TYPE" in
-    dock|desktop|notification)  RULE+=("manage=off")    ;;
-    toolbar|utility)            RULE+=("focus=off")     ;;
-    desktop)                    RULE+=("lower=on")      ;;
-esac
-
-for s in $STATE; do
-    case $s in
-        sticky)                 RULE+=("sticky=on")     ;;
-        fullscreen)             RULE+=("fullscreen=on") ;;
-        modal)                  RULE+=("floating=on") ;;
-    esac
-done
-
-case "$INST" in
+case "$(xwinfo -i $1)" in
     pithos)
-        #RULE+=("desktop=$([[ $(bspc query -M | wc -l) -gt 1 ]] && echo '^1' || echo 'next.free')") ;;
-        RULE+=("desktop=1/Ten") ;;
+        #echo "desktop=$([[ $(bspc query -M | wc -l) -gt 1 ]] && echo '^1' || echo 'next.free')" ;;
+        echo "desktop=1/Ten" ;;
     mpv|keepassx)
-        RULE+=("floating=on") ;;
+        echo "floating=on" ;;
     dropterm|crx_nckgahadagoaajjgafhacjanaoiihapd)
-        RULE+=("floating=on sticky=on") ;;
+        echo "floating=on sticky=on" ;;
 esac
-
-echo "${RULE[*]}"
